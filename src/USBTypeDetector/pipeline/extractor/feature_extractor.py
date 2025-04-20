725dcdcffd688341d79bb04f2c5d2e434aa5363f
Seif Yasser <21p0102@eng.asu.edu.eg>
2025-04-20 17:35:24 +0200

    feat: add FeatureExtractor class with placeholder methods for corner, edge, and morphological operations

---
+++ src/USBTypeDetector/pipeline/extractor/feature_extractor.py
@@ -0,0 +1,16 @@
+import cv2
+from preprocessor import image_preprocessor
+
+
+class FeatureExtractor:
+    def __init__(self):
+        pass
+
+    def corner_detection():
+        pass
+
+    def edge_detection():
+        pass
+
+    def morphological_operations():
+        pass
